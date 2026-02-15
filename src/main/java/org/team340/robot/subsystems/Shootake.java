7da3ba390095bd5c83f38bb8aab1bb457f728194
natecummings18 <nathancummings1822@gmail.com>
2026-02-14 19:21:09 -0500

    adjuste pid, make speed tunable

--- src/main/java/org/team340/robot/subsystems/Shootake.java
+++ src/main/java/org/team340/robot/subsystems/Shootake.java
@@ -10,11 +10,14 @@ import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
+import org.team340.lib.tunable.TunableTable;
+import org.team340.lib.tunable.Tunables;
+import org.team340.lib.tunable.Tunables.TunableDouble;
 import org.team340.lib.util.command.GRRSubsystem;
 import org.team340.robot.Constants.RobotMap;
 
@@ -22,26 +25,30 @@ public class Shootake extends GRRSubsystem {
 
     private final SparkMax hopper;
     private final SparkMax shooter;
+
+    private static final TunableTable tunables = Tunables.getNested("shooter");
+    private static final TunableDouble shootingSpeed = tunables.value("velocity", 2000.0);
     SparkClosedLoopController shooterController;
+
     /** Creates a new Shootake. */
     public Shootake() {
         hopper = new SparkMax(RobotMap.HOPPER_MOTOR, MotorType.kBrushless);
         shooter = new SparkMax(RobotMap.INTAKE_SHOOTER_MOTOR, MotorType.kBrushless);
         SparkBaseConfig shooterConfig = new SparkMaxConfig();
-        shooterConfig.voltageCompensation(12)
-                .idleMode(IdleMode.kCoast)
-                .closedLoop
-                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                .pid(0.01, 0, 0)
-                .feedForward.kS(0.0)
-                .kV(.13);
+        shooterConfig
+            .voltageCompensation(12)
+            .idleMode(IdleMode.kCoast)
+            .closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .pid(0.01, 0, 0)
+            .feedForward.kS(0.0)
+            .kV(.13);
         shooterConfig.encoder
-                .positionConversionFactor(1.0)
-                .velocityConversionFactor(1.0 / 60.0)
-                .uvwMeasurementPeriod(16)
-                .uvwAverageDepth(2);
-        hopperConfig.voltageCompensation(12)
-                .idleMode(IdleMode.kBrake)       
+            .positionConversionFactor(1.0)
+            .velocityConversionFactor(1.0)
+            .uvwMeasurementPeriod(16)
+            .uvwAverageDepth(2);
+        SparkBaseConfig hopperConfig = new SparkMaxConfig();
+        hopperConfig.voltageCompensation(12).idleMode(IdleMode.kBrake);
         shooter.configure(shooterConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
         shooterController = shooter.getClosedLoopController();
     }
@@ -78,13 +85,13 @@ public class Shootake extends GRRSubsystem {
     public Command shoot() {
         return commandBuilder()
             .onExecute(() -> {
-                shooterController.setSetpoint(2, ControlType.kVelocity);
+                shooterController.setSetpoint(shootingSpeed.getAsDouble(), ControlType.kVelocity);
             })
             .withTimeout(.5)
             .andThen(
                 commandBuilder()
                     .onExecute(() -> {
-                        shooterController.setSetpoint(2, ControlType.kVelocity);
+                        shooterController.setSetpoint(shootingSpeed.getAsDouble(), ControlType.kVelocity);
                         hopper.set(-1);
                     })
                     .onEnd(() -> {
